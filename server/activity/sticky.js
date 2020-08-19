5c582100b2feca934c82e60967e1b1c2507faba0
kdvedi321 <himanshudwivedi99@gmail.com>
2020-08-19 20:32:13 +0530

    changed app and package for deployment

--- server/activity/sticky.js
+++ server/activity/sticky.js
@@ -1,4 +1,13 @@
-function createSticky(){
+function createSticky() {
+    // <div class="stickyPad">
+    //     <div class="nav-bar">
+    //         <div class="close"></div>
+    //         <div class="minimize"></div>
+    //     </div>
+    //     <div class="textbox">
+    //         <textarea name="" id="" cols="30" rows="10"></textarea></div>
+    // </div>
+    // create 
     let textBox = createBox();
     let textarea = document.createElement("textarea");
     textBox.appendChild(textarea);
--- server/activity/utility.js
+++ server/activity/utility.js
@@ -1,61 +1,66 @@
-function createBox(){
+function createBox() {
     let stickyPad = document.createElement("div");
     let navBar = document.createElement("div");
     let close = document.createElement("div");
     let minimize = document.createElement("div");
     let textbox = document.createElement("div");
-    // add classes
-    stickyPad.setAttribute("class","stickyPad");
-    navBar.setAttribute("class","nav-bar");
-    close.setAttribute("class","close");
-    minimize.setAttribute("class","minimize");
-    textbox.setAttribute("class","textbox");
+    //    add classes
+    stickyPad.setAttribute("class", "stickyPad");
+    navBar.setAttribute("class", "nav-bar");
+    close.setAttribute("class", "close");
+    minimize.setAttribute("class", "minimize");
+    textbox.setAttribute("class", "textbox");
     // create subtree
     stickyPad.appendChild(navBar);
     stickyPad.appendChild(textbox);
     navBar.appendChild(minimize);
     navBar.appendChild(close);
-    // textbox.appendChild(textarea);
     // add subtree to page
     document.body.appendChild(stickyPad);
-    close.addEventListener("click", function(){
+    // close=> remove 
+    close.addEventListener("click", function () {
         stickyPad.remove();
     })
-    let isOpen = true;
-    minimize.addEventListener("click", function(){
-        if(isOpen){
+    let isOpen = true
+    // minimize=> 
+    minimize.addEventListener("click", function () {
+        if (isOpen) {
             textbox.style.display = "none";
-        }else{
+        } else {
             textbox.style.display = "block";
         }
         isOpen = !isOpen;
     })
+    //  move => draw
     let initialX = null;
     let initialY = null;
     let isStickyDown = false;
-    navBar.addEventListener("mousedown", function(e){
+    navBar.addEventListener("mousedown", function (e) {
         initialX = e.clientX;
         initialY = e.clientY;
-        isStickyDown = true;       
+        isStickyDown = true
     })
-    navBar.addEventListener("mousemove", function(e){
-        if(isStickyDown == true){
+    navBar.addEventListener("mousemove", function (e) {
+        if (isStickyDown == true) {
             let finalX = e.clientX;
             let finalY = e.clientY;
             let dX = finalX - initialX;
             let dY = finalY - initialY;
-            let{ top, left } = stickyPad.getBoundingClientRect();
+            //  
+            let { top, left } = stickyPad.getBoundingClientRect();
             stickyPad.style.top = top + dY + "px";
             stickyPad.style.left = left + dX + "px";
             initialX = finalX;
             initialY = finalY;
         }
     })
-    navBar.addEventListener("mouseup", function(e){
-        isStickyDown = false;
+    //  navBar => mouse pointer up 
+    navBar.addEventListener("mouseup", function (e) {
+        isStickyDown = false
     })
-    navBar.addEventListener("mouseleave", function(e){
-        isStickyDown = false;
+    //  
+    navBar.addEventListener("mouseleave", function (e) {
+        isStickyDown = false
     })
     return textbox;
 }
